32161737902532f1efe48f0e9bf7fa10f41858b1
Lindsay Haven <lindsay.haven@gmail.com>
2014-08-24 10:09:31 -0700

    Adding ambient map audio again
    Adding ambient map audio back to the game.

--- map.js
+++ map.js
@@ -117,6 +117,7 @@ define(['lib/crafty', 'constants', 'lib/tiledmapbuilder', 'props'], function (Cr
 
       this._lightDone = false;
       this._darkDone = false;
+      Crafty.audio.play("ambient", -1);
     },
     showLight: function () {
       console.log("Light transition triggered.");
